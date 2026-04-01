ac0c01911a53363d966d26915a58ca7303702303
ElmoCrumley <MW826124@gmail.com>
2026-04-01 13:34:48 +0300

    Немного изменить обработчик ошибок и возвращать именно объект с текстом в теле

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,27 +1,24 @@
 package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleNotFoundException(NotFoundException e) {
-        return e.getMessage();
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleConflict(Conflict e) {
-        return e.getMessage();
+    @ExceptionHandler(Conflict.class)
+    public ResponseEntity<String> handleConflict(Conflict e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleInternalServerError(InternalServerError e) {
-        return e.getMessage();
+    @ExceptionHandler(InternalServerError.class)
+    public ResponseEntity<String> handleInternalServerError(InternalServerError e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
