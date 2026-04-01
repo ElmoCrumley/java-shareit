61dc73738d7922bf986f6f19b2b0f2ec8ab897e2
ElmoCrumley <MW826124@gmail.com>
2026-04-01 16:03:37 +0300

    Убрать публичные модификаторы доступа. Класс-реализация находится в той же папке

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -4,13 +4,13 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface UserService {
-    public Optional<User> create(User user);
+    Optional<User> create(User user);
 
-    public Collection<User> findAll();
+    Collection<User> findAll();
 
-    public Optional<User> findById(Long userId);
+    Optional<User> findById(Long userId);
 
-    public Optional<User> update(User user);
+    Optional<User> update(User user);
 
-    public void delete(Long userId);
+    void delete(Long userId);
 }
