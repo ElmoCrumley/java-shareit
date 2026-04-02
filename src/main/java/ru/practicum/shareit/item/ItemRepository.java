093d1bc77b0fb0a134f8c3afe276120f254f7316
ElmoCrumley <MW826124@gmail.com>
2026-04-02 17:02:29 +0300

    Создать ItemRepository для CRUD запросов и его реализацию ItemRepositoryImpl. Новым было использовать stream для поиска по двум параметром и использовать || в фильтрации данных в методе search и использование в stream Optional.ofNullable в случае null в поле передаваемого объекта. Удобно.

---
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.item;
+
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface ItemRepository {
+    Optional<Item> create(Long userId, ItemDto itemDto);
+
+    Collection<Item> findAll(Long userId);
+
+    Optional<Item> findById(Long userId, Long itemId);
+
+    Optional<Item> update(Long userId, ItemDto itemDto, Long itemId);
+
+    Collection<Item> search(Long userId, String text);
+}
---
+++ src/main/java/ru/practicum/shareit/item/ItemRepositoryImpl.java
@@ -0,0 +1,72 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.*;
+
+@Repository
+public class ItemRepositoryImpl implements ItemRepository {
+    private final Map<Long, Item> items = new HashMap<>();
+
+    @Override
+    public Optional<Item> create(Long userId, ItemDto itemDto) {
+        Item item = new Item();
+
+        item.setId(getNextId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setUserId(userId);
+        items.put(item.getId(), item);
+        return Optional.of(item);
+    }
+
+    @Override
+    public Collection<Item> findAll(Long userId) {
+        return items.values().stream()
+                .filter(item -> item.getUserId().equals(userId))
+                .toList();
+    }
+
+    @Override
+    public Optional<Item> findById(Long userId, Long itemId) {
+        return Optional.ofNullable(items.get(itemId));
+    }
+
+    @Override
+    public Optional<Item> update(Long userId, ItemDto itemDto, Long itemId) {
+        Item storageItem = items.get(itemId);
+        if (storageItem == null) return Optional.empty();
+
+        storageItem.setName(itemDto.getName());
+        storageItem.setDescription(itemDto.getDescription());
+        storageItem.setAvailable(itemDto.getAvailable());
+        return Optional.of(storageItem);
+    }
+
+    @Override
+    public Collection<Item> search(Long userId, String text) {
+        return items.values().stream()
+                .filter(item -> Optional.ofNullable(item.getAvailable()).orElse(false)
+                        && (Optional.ofNullable(item.getName())
+                                                .map(name -> name.toLowerCase().contains(text.toLowerCase()))
+                                                .orElse(false)
+                                || Optional.ofNullable(item.getDescription())
+                                                .map(name -> name.toLowerCase().contains(text.toLowerCase()))
+                                                .orElse(false)
+                        )
+                )
+                .toList();
+    }
+
+    private long getNextId() {
+        long currentMaxId = items.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
