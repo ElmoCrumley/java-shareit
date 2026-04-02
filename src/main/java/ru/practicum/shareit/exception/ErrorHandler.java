e3315635fa23109639d1825cd2d00f33343d105a
ElmoCrumley <MW826124@gmail.com>
2026-04-02 12:37:52 +0300

    Изменить обработку исключений. Теперь возвращается json с описанием ошибки прямо в postman. Это оказывается гораздо удобнее.

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,29 +1,61 @@
 package ru.practicum.shareit.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler(BadDataBody.class)
-    public ResponseEntity<String> handleBadDataBody(BadDataBody e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<Object> handleBadDataBody(BadDataBody e) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("error", e.getMessage());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException e) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("error", e.getMessage());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 
     @ExceptionHandler(Conflict.class)
-    public ResponseEntity<String> handleConflict(Conflict e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    public ResponseEntity<Object> handleConflict(Conflict e) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("error", e.getMessage());
+        body.put("status", HttpStatus.CONFLICT.value());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 
     @ExceptionHandler(InternalServerError.class)
-    public ResponseEntity<String> handleInternalServerError(InternalServerError e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    public ResponseEntity<Object> handleInternalServerError(InternalServerError e) {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("error", e.getMessage());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
     }
 }
