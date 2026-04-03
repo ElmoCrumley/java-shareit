0abfe3d28e6fb5c74720a2a42a04bca96cb25108
ElmoCrumley <MW826124@gmail.com>
2026-04-03 16:34:44 +0300

    Исправить "надо дополнить классы дто необходимыми полями, например тут не хватает поля с почтой, так как у пользователя email обязателен и уникален"

--- src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserMapper.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.user.dto;
 import ru.practicum.shareit.user.User;
 
 public class UserMapper {
-    public UserDto toDtoUser(User user) {
+    public static UserDto toDtoUser(User user) {
         return UserDto.builder()
                 .name(user.getName())
                 .build();
