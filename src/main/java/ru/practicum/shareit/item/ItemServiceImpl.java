6b7b04217880f99f72f8ce5ff9ea8d67266e9317
ElmoCrumley <MW826124@gmail.com>
2026-04-06 16:22:47 +0300

    Исправить "Это выглядит “надежно”, но на деле ломает поведение приложения. Во-первых, ты убиваешь стек-трейс..." Убрал все try-catch. Спасибо за подробные объяснения ошибок.

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -24,81 +24,57 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemDto create(Long userId, Item item) {
-        try {
-            Optional<Item> storageItem = itemRepository.create(userId, item);
+        Optional<Item> storageItem = itemRepository.create(userId, item);
 
-            if (storageItem.isPresent()) {
-                return ItemMapper.toDtoItem(storageItem.get());
-            } else {
-                throw new ItemCreationException("Item can't be created");
-            }
-        } catch (Exception e) {
-            throw new InternalServerError(e.getMessage());
+        if (storageItem.isPresent()) {
+            return ItemMapper.toDtoItem(storageItem.get());
+        } else {
+            throw new ItemCreationException("Item can't be created");
         }
     }
 
     @Override
     public Collection<ItemDto> findAll(Long userId) {
-        try {
-            return itemRepository.findAll(userId).stream()
-                    .filter(item -> item.getUserId().equals(userId))
-                    .map(ItemMapper::toDtoItem)
-                    .toList();
-        } catch (Exception e) {
-            throw new InternalServerError(e.getMessage());
-        }
+        return itemRepository.findAll(userId).stream()
+                .filter(item -> item.getUserId().equals(userId))
+                .map(ItemMapper::toDtoItem)
+                .toList();
     }
 
     @Override
     public ItemDto findById(Long userId, Long itemId) {
-        try {
-            Optional<Item> item = itemRepository.findById(userId, itemId);
+        Optional<Item> item = itemRepository.findById(userId, itemId);
 
-            if (item.isPresent()) {
-                return ItemMapper.toDtoItem(item.get());
-            } else {
-                throw new NotFoundException("Item wasn't found");
-            }
-        } catch (NotFoundException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalServerError(e.getMessage());
+        if (item.isPresent()) {
+            return ItemMapper.toDtoItem(item.get());
+        } else {
+            throw new NotFoundException("Item wasn't found");
         }
     }
 
     @Override
     public ItemDto update(Long userId, ItemDto itemDto, Long itemId) {
-        try {
-            Optional<Item> item = itemRepository.update(userId, itemDto, itemId);
+        Optional<Item> item = itemRepository.update(userId, itemDto, itemId);
 
-            if (item.isPresent()) {
-                return ItemMapper.toDtoItem(item.get());
-            } else {
-                throw new NotFoundException("Item wasn't updated");
-            }
-        } catch (NotFoundException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new InternalServerError(e.getMessage());
+        if (item.isPresent()) {
+            return ItemMapper.toDtoItem(item.get());
+        } else {
+            throw new NotFoundException("Item wasn't updated");
         }
     }
 
     public Collection<ItemDto> search(Long userId, String text) {
-        try {
-            return itemRepository.findAll(userId).stream()
-                    .filter(item -> Optional.ofNullable(item.getAvailable()).orElse(false)
-                                    && (Optional.ofNullable(item.getName())
-                                    .map(name -> name.toLowerCase().contains(text.toLowerCase()))
-                                    .orElse(false)
-                                    || Optional.ofNullable(item.getDescription())
-                                    .map(name -> name.toLowerCase().contains(text.toLowerCase()))
-                                    .orElse(false)
-                            )
-                    )
-                    .map(ItemMapper::toDtoItem)
-                    .toList();
-        } catch (Exception e) {
-            throw new InternalServerError(e.getMessage());
-        }
+        return itemRepository.findAll(userId).stream()
+                .filter(item -> Optional.ofNullable(item.getAvailable()).orElse(false)
+                                && (Optional.ofNullable(item.getName())
+                                .map(name -> name.toLowerCase().contains(text.toLowerCase()))
+                                .orElse(false)
+                                || Optional.ofNullable(item.getDescription())
+                                .map(name -> name.toLowerCase().contains(text.toLowerCase()))
+                                .orElse(false)
+                        )
+                )
+                .map(ItemMapper::toDtoItem)
+                .toList();
     }
 }
