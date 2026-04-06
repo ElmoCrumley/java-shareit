06047789eaedeba56327df91c2bbbff5faf244db
ElmoCrumley <MW826124@gmail.com>
2026-04-06 16:23:35 +0300

    Лишний импорт

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item;
 
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.exception.InternalServerError;
 import ru.practicum.shareit.exception.ItemCreationException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
